e448c78a5d0bdd5e3a57ca018c954da4e2fa16a7
rawatsanjaydirtcube <65915348+rawatsanjaydirtcube@users.noreply.github.com>
2024-01-11 17:12:10 +0530

    Comments on grid class

--- Assets/Scripts/Grid/GridCell.cs
+++ Assets/Scripts/Grid/GridCell.cs
@@ -6,11 +6,13 @@ using TicTacToe.Constants;
 
 namespace TicTacToe.Grid
 {
+    /// <summary>
+    /// This script is attached to each grid cell this holds data for this grid cell
+    /// </summary>
     public class GridCell : GridCellBase, IGridData
     {
         public string m_GridAcquiredBy;    
         public string CurrentOccupant => m_GridAcquiredBy;
-
         public int GetGridCellId => m_gridIdInInt;
 
         private GameManager m_gameManager;
@@ -20,11 +22,15 @@ namespace TicTacToe.Grid
 
         private void OnDisable() => UnRegisterToGameManager();
         
-        private void Start()
-        {
-            m_ScoreManager = FindObjectOfType<TicTacToeScoreManager>();
-        }
+        private void Start() => m_ScoreManager = FindObjectOfType<TicTacToeScoreManager>();
 
+        public void RegisterToGameManager() => GameManager.RegisterGridBase(this);
+
+        public void UnRegisterToGameManager() => GameManager.UnRegisterGridBase(this);
+
+        /// <summary>
+        /// When someone clicks on this grid this data is populated based on the game type then further action is taken
+        /// </summary>
         public void PopulateData()
         {
             if (m_gameManager.gameType == GamePlayType.AI)            
@@ -33,6 +39,9 @@ namespace TicTacToe.Grid
                  MultiplayerMove();            
         }
 
+        /// <summary>
+        /// If this is of type against AI then the player move grabs data from manager to full
+        /// </summary>
         private void PlayerMove()
         {
             if (m_gameManager.playerMove)
@@ -45,6 +54,9 @@ namespace TicTacToe.Grid
             }
         }
 
+        /// <summary>
+        /// If this is of type against Multiplayer then the player move grabs data from manager to full
+        /// </summary>
         private void MultiplayerMove()
         {
             playerSide = m_ScoreManager.GetPlayerSide(GameConstants.K_CurrentPlayerName);
@@ -53,6 +65,10 @@ namespace TicTacToe.Grid
             m_gameManager.EndTurn(m_gridIdInInt, playerSide);
         }
 
+        /// <summary>
+        /// The Game manager registers himself to particular grid so that he can have reference for that
+        /// </summary>
+        /// <param name="manager"></param>
         public void SetGameControllerReference(GameManager manager ) => m_gameManager = manager;
 
         public override void SetDelegate(IGridData gridDelegate)
@@ -60,9 +76,6 @@ namespace TicTacToe.Grid
             GridDelegate = gridDelegate;
         }
 
-        public void RegisterToGameManager() => GameManager.RegisterGridBase(this);       
-
-        public void UnRegisterToGameManager() => GameManager.UnRegisterGridBase(this);
     }
 
 
